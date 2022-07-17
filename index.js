2ed911f067f73034e0d83cca1260bec5091bda79
cultstatue <saltedchurros@gmail.com>
2022-07-17 18:17:54 -0400

    added validation for questions

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// TODO: Include packages needed for this application
+// require neccessary modules
 const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
+// function to prompt user for information to generate readme
 const promptReadMe = () => {
 
     return inquirer.prompt ([
@@ -9,49 +9,123 @@ const promptReadMe = () => {
         {
             type: 'input',
             name: 'github',
-            message:'Please enter your github username'
+            message:'Please enter your github username',
+            validate: githubInput => {
+                if (githubInput) {
+                  return true;
+                } else {
+                  console.log('Please enter a github username!');
+                  return false;
+                }
+            }
         },
 
         {
             type: 'input',
             name: 'email',
-            message: 'Please enter your email address'
+            message: 'Please enter your email address',
+            validate: emailInput => {
+                if (emailInput) {
+                  return true;
+                } else {
+                  console.log('Please enter an email address!');
+                  return false;
+                }
+            }
+            
         },
         {
             type: 'input',
             name: 'title',
-            message: 'Please enter the title of your project'
+            message: 'Please enter the title of your project',
+            validate: titleInput => {
+                if (titleInput) {
+                  return true;
+                } else {
+                  console.log('Please enter a title for your project!');
+                  return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Please enter a description of your project'
+            message: 'Please enter a description of your project',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                  return true;
+                } else {
+                  console.log('Please enter a description of your project!');
+                  return false;
+                }
+            }
+            
         },
         {
             type: 'input',
             name: 'installation',
-            message: 'Please enter installation instructions for your project'
+            message: 'Please enter installation instructions for your project',
+            validate: installationInput => {
+                if (installationInput) {
+                  return true;
+                } else {
+                  console.log('Please enter installation instructions for your project!');
+                  return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'usage',
-            message: 'Please enter usage instructions for your project'
+            message: 'Please enter usage instructions for your project',
+            validate: usageInput => {
+                if (usageInput) {
+                  return true;
+                } else {
+                  console.log('Please enter usage instructions for your project!');
+                  return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'contribution',
-            message: 'Please enter contribution guidelines and rules for your project'
+            message: 'Please enter contribution guidelines and rules for your project',
+            validate: contributionInput => {
+                if (contributionInput) {
+                  return true;
+                } else {
+                  console.log('Please enter contribution instructions for your project!');
+                  return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'tests',
-            message: 'Please enter any tests and resolved examples for your project'
+            message: 'Please enter any tests and resolved examples for your project',
+            validate: testsInput => {
+                if (testsInput) {
+                  return true;
+                } else {
+                  console.log('Please enter test instructions for your project!');
+                  return false;
+                }
+            }
         },
         {
             type: 'list',
-            name: 'licence',
+            name: 'license',
             message: 'Please choose a license for your project(select one)',
-            choices: ['No License', 'MIT', 'GPLv3']
+            choices: ['No License', 'MIT', 'GPLv3'],
+            validate: licenseInput => {
+                if (licenseInput) {
+                  return true;
+                } else {
+                  console.log('Please choose a licesne option!');
+                  return false;
+                }
+            }
         }
 
     ])
